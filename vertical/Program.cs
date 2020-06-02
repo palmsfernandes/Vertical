78d3803ca8b5a728b81f8103207b1d0a573e320d
Vinicius Fernandes <vinicius.fernandes@bunzlbrasil.protcapad.com.br>
2020-06-02 18:08:15 -0300

    Novo

---
+++ vertical/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vertical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            int tamanho;
+
+            Console.Write("Escreva uma palavra: ");
+            a = Console.ReadLine().ToUpper();
+            tamanho = a.Length;
+
+            for (int i = 0; i <= tamanho; i++)
+            {
+                Console.WriteLine(a.Substring(0, i));
+            }
+
+            for (int i = 0; i <= tamanho; i++)
+            {
+                Console.WriteLine(a.Substring(i));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
